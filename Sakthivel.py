289f816d749147bf8b61d52b0262561254e2b398
Ari Sakthivel <sakthivel12514@gmail.com>
2024-01-09 17:21:40 +0530

    Git Automation codes

--- Sakthivel.py
+++ Sakthivel.py
@@ -2,9 +2,16 @@ from selenium import webdriver
 # import chromedriver_autoinstaller
 from selenium.webdriver.chrome.service import Service
 
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+capabilities = {'pageLoadStrategy': 'normal', 'pageLoadTimeout': 60000}  # Set timeout in milliseconds
+executable_path = "drivers/chromedriver.exe"
+driver = webdriver.Chrome(service=Service("drivers/chromedriver.exe"), options=options, desired_capabilities=capabilities, service_args=['--verbose', '--log-path=/path/to/chromedriver.log'])
 
 
-driver = webdriver.Chrome()
+# driver = webdriver.Chrome()
 # executable_path = "drivers/chromedriver.exe"
 # driver = webdriver.Chrome(service=Service(executable_path))
 # executable_path = chromedriver_autoinstaller.install()
