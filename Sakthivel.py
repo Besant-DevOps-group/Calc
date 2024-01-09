85e88d245a81e9ec1861fef0c710cb6c65c888df
Ari Sakthivel <sakthivel12514@gmail.com>
2024-01-09 14:56:52 +0530

    Git Automation code

--- Sakthivel.py
+++ Sakthivel.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
+import chromedriver_autoinstaller
+from selenium.webdriver.chrome.service import Service
 
-driver = webdriver.Firefox()
+
+executable_path = chromedriver_autoinstaller.install()
+driver = webdriver.Chrome(service=Service(executable_path))
 driver.maximize_window()
 
 driver.get("https://github.com/")
---
+++ Sakthivel_Test.py
@@ -0,0 +1,7 @@
+import Sakthivel as fileData
+from selenium.webdriver.common.by import By
+
+textValue = fileData.driver.find_element(By.XPATH, "/html/body/div[1]/div[4]/main/div[1]/div[2]/div/div/div[2]/h1/span").text
+# print(textValue)
+assert textValue == "Let’s build from here"
+fileData.driver.quit()
