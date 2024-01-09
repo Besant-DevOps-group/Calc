7c67a76b5cb05b8d48cd40b7d17338989dd44264
Ari Sakthivel <sakthivel12514@gmail.com>
2024-01-09 15:06:07 +0530

    Git Automation code

--- Sakthivel.py
+++ Sakthivel.py
@@ -3,8 +3,9 @@ import chromedriver_autoinstaller
 from selenium.webdriver.chrome.service import Service
 
 
-executable_path = chromedriver_autoinstaller.install()
-driver = webdriver.Chrome(service=Service(executable_path))
+# executable_path = chromedriver_autoinstaller.install()
+# driver = webdriver.Chrome(service=Service(executable_path))
+driver =webdriver.Firefox()
 driver.maximize_window()
 
 driver.get("https://github.com/")
