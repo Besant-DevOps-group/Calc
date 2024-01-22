7b13e7fe995d52053d5749a3083eb4d2b637a448
Vignesh2308m <vicky.manoharan2018@gmail.com>
2024-01-22 08:15:34 +0530

    Test Jenkins pipeline

--- main.py
+++ main.py
@@ -3,3 +3,6 @@
 #Addition function a=5,b=5 output: 10
 def add(a,b):
     return a+b
+
+def sub(a,b):
+    return a-b
--- main_test.py
+++ main_test.py
@@ -1,9 +1,11 @@
 import unittest
-from main import add
+from main import add,sub
 
 class TestCalc(unittest.TestCase):
     def testadd(self):
         return self.assertEqual(add(3,2),5)
+    def testsub(self):
+        return self.assertEqual(sub(3,2),1)
     
 
 
